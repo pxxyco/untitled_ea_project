b73922ec1678a324469bbf93d4739e86b9d7b58a
Yorshkrum <antoniofranco325@gmail.com>
2026-07-19 19:07:42 +0200

    Aggiunte classi JPA Activity, ActivityImage e TripImage, da rimuovere commento sul campo trip_id

---
+++ ea-project/src/main/java/it/unical/ea_project/domain/ActivityImage.java
@@ -0,0 +1,30 @@
+package it.unical.ea_project.domain;
+
+import it.unical.ea_project.domain.Activity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "ACTIVITY_IMAGE")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActivityImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long imageId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "activity_id", nullable = false)
+    private Activity activity;
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    @Column(name = "order_index")
+    private Integer orderIndex;
+}
