36ebc4391401ab17d65c38c7ba99541bc61b9bd5
lucoilbasso <221305859+lucoilbasso@users.noreply.github.com>
2026-07-20 16:37:40 +0200

    Creazioni Classi ListActivities e ListSharing

---
+++ ea-project/src/main/java/it/unical/ea_project/domain/ListActivities.java
@@ -0,0 +1,37 @@
+package it.unical.ea_project.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "favoriteList")
+
+@Entity
+@IdClass(ListActivities.ListActivityId.class)
+@Table(name = "\"List_Activities\"")
+public class ListActivities {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "list_id", nullable = false)
+    private FavoriteList favoriteList;
+
+    @Id
+    @Column(name = "activity_id", nullable = false)
+    private Long activityId;
+
+    @Column(name = "added_at")
+    private LocalDateTime addedAt;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ListActivityId implements Serializable {
+        private Long favoriteList;
+        private Long activityId;
+    }
+}
