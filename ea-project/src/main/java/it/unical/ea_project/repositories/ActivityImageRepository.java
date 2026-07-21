3d46e5fe823c246b2886b2fd0b012fd34a69caff
Yorshkrum <antoniofranco325@gmail.com>
2026-07-21 16:05:28 +0200

    creati repository delle classi Activity, Activity Image, TripImage, User

---
+++ ea-project/src/main/java/it/unical/ea_project/repositories/ActivityImageRepository.java
@@ -0,0 +1,18 @@
+package it.unical.ea_project.repositories;
+
+
+import it.unical.ea_project.domain.Activity;
+import it.unical.ea_project.domain.ActivityImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ActivityImageRepository extends JpaRepository<ActivityImage, Long> {
+
+    // trova tutte le immagini associate a una specifica attività, ordinate per orderIndex
+    List<ActivityImage> findByActivityOrderByOrderIndexAsc(Activity activity);
+
+    // rimuove tutte le immagini legate a un'attività
+    void deleteByActivity(Activity activity);
+}
---
+++ ea-project/src/main/java/it/unical/ea_project/repositories/UserRepository.java
@@ -0,0 +1,21 @@
+package it.unical.ea_project.repositories;
+
+import it.unical.ea_project.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // cercare un utente per email
+    Optional<User> findByEmail(String email);
+
+    // cercare un utente per username
+    Optional<User> findByUsername(String username);
+
+    // verificare se un'email o username esistono già
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+}
