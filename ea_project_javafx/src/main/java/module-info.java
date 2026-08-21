12bd52aabb12a88611a951f1b0e640f1d1c79c3b
brunolen <212409209+BrunoStackDev@users.noreply.github.com>
2026-08-21 11:41:12 +0200

    fix(config): fix pom.xml , module java

--- ea_project_javafx/src/main/java/module-info.java
+++ ea_project_javafx/src/main/java/module-info.java
@@ -9,8 +9,6 @@ module it.unical.ea_project_javafx {
     opens it.unical.ea_project_javafx to javafx.fxml;
     opens it.unical.ea_project_javafx.controller to javafx.fxml, com.google.gson;
     opens it.unical.ea_project_javafx.dto to com.google.gson;
-
-    opens it.unical.ea_project_javafx.controller to javafx.fxml;
     opens it.unical.ea_project_javafx.controller.attivita to javafx.fxml;
     opens it.unical.ea_project_javafx.controller.attivita.fotoContainer to javafx.fxml;
     opens it.unical.ea_project_javafx.controller.attivita.recensioni to javafx.fxml;
